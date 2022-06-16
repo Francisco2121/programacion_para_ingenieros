57eff37bd4fa1212f77157aa4bdab7bf85aeaaec
Francisco <107285062+Francisco2121@users.noreply.github.com>
2022-06-16 05:08:58 -0500

    Add files via upload

---
+++ Laboratories/Francisco21.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+interes=100*(1+0.10)**7
+print(interes)
+
+
+# In[4]:
+
+
+ahorro=100
+print(ahorro)
+
+
+# In[5]:
+
+
+ahorro=100
+mult_anual=(1.1)
+result=ahorro*(mult_anual)**7
+print(result)
+
+
+# In[9]:
+
+
+desc=str("cadena de textos")
+print(desc)
+profit=bool(True)
+print(profit)
+
+
+# In[56]:
+
+
+ahorro=100
+mult=1.1
+desc='interes compuesto'
+año1=ahorro*(mult)
+print(año1)
+print(type(año1))
+dobledesc=desc+desc
+print(dobledesc)
+
+
+# In[72]:
+
+
+ahorro=100
+resultado=100*(1.1)**7
+print("Empezamos con "+ str(𝑎ℎ𝑜𝑟𝑟𝑜) + "y ahora mismo tengo " + str(resultado) + ". Que bien!")
+pi_string = "3.1415926"
+pi_float=float(pi_string)
+print(pi_float)
+
